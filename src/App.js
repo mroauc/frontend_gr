a35dcf036f88513e0b806dd460f3a7b41e0584f9
rodrigovm7 <rodrigovm7@gmail.com>
2021-04-19 10:01:29 -0400

    cambios en sintaxis

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import 'bootstrap/dist/js/bootstrap';
 class App extends Component {
   render(){
     localStorage.setItem('url',"104.236.118.82:8080");
-<<<<<<< HEAD
-    // localStorage.setItem('url', "http://localhost:8080");
-=======
     // localStorage.setItem('url',"http://localhost:8080");
->>>>>>> d8f10c80cbb47161e9dd51f3ee8bbe89c296fef0
     return (
       
       <div className="App">
